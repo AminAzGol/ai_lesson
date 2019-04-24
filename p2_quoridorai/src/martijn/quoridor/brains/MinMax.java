80dabd4d6d1d2b15d51d5a207281ef3fac4f0400
mohi <ansari0512@yahoo.com>
2019-04-24 09:54:41 +0430

    before fix

--- p2_quoridorai/src/martijn/quoridor/brains/MinMax.java
+++ p2_quoridorai/src/martijn/quoridor/brains/MinMax.java
@@ -30,15 +30,15 @@ public class MinMax {
         else{
             for (int i = 0; i < childes.size(); i++) {
                 Node retval = min_max(childes.get(i).getKey(), !i_am_min, horizon - 1, alpha, beta);
-//                if(i_am_min && retval.cost < beta){
-//                    beta  = retval.cost;
-//                }
-//                else if (retval.cost > alpha){
-//                    alpha = retval.cost;
-//                }
-//                if (alpha > beta){
-//                    return retval;
-//                }
+                if(i_am_min && retval.cost < beta){
+                    beta  = retval.cost;
+                }
+                else if (retval.cost > alpha){
+                    alpha = retval.cost;
+                }
+                if (alpha > beta){
+                    return retval;
+                }
                 Node mychild = new Node(childes.get(i).getKey(),childes.get(i).getValue());
                 mychild.setCost(retval.cost);
                 queue.add(mychild);
