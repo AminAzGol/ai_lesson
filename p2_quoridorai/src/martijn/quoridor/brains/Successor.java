1f0f49fbe1972f0f3fc8f38ffdab20847b145b67
aminag <ma.alizadeh.gol@gmail.com>
2019-04-25 16:25:09 +0430

    change succ

--- p2_quoridorai/src/martijn/quoridor/brains/Successor.java
+++ p2_quoridorai/src/martijn/quoridor/brains/Successor.java
@@ -9,39 +9,41 @@ import java.util.Set;
 
 
 public class Successor {
-    public static ArrayList<Pair<Board,Move>> successor(Board board){
+    public static ArrayList<Node> successor(Board board){
         Player player = board.getTurn();
         Board tempBoard ;
         tempBoard =  board.clone();
-        ArrayList<Pair<Board,Move>> childern = new ArrayList<>();
+        ArrayList<Node>  childern = new ArrayList<>();
         Set<Position> jumps = player.getJumpPositions();
-        if(jumps.size() == 0)
-            System.out.printf("em");
-        for (Position jump : jumps){
-            tempBoard =  board.clone();
-            Jump move = new Jump(jump);
-            tempBoard.move(move);
-            childern.add(new Pair<> (tempBoard,move));
-        }
+//        for (Position jump : jumps){
+//            tempBoard =  board.clone();
+//            Jump move = new Jump(jump);
+//            tempBoard.move(move);
+//            childern.add(new Pair<> (tempBoard,move));
+//        }
+        tempBoard =  board.clone();
+        tempBoard.move(new Jump(player.stepToGoal()));
+        childern.add(new Node(tempBoard,new Jump(player.stepToGoal())));
+
         for (int i=0; i< board.getWidth() ; i++){
             for (int j=0; j <board.getHeight();j++){
                 Position position = new Position(j,i);
                 if (tempBoard.containsWallPosition(position)) {
-
                     if (tempBoard.getWall(position) == null) {
                         tempBoard = board.clone();
                         tempBoard.setWall(position, Wall.HORIZONTAL);
                         PutWall move = new PutWall(position, Wall.HORIZONTAL);
+//                        PutWall move = new PutWall(position, Wall.HORIZONTAL);
                         if (move.isLegal(board)){
                             tempBoard.move(move);
-                            childern.add(new Pair<>(tempBoard, move));
+                            childern.add(new Node(tempBoard, move));
                         }
 
                         tempBoard = board.clone();
                         move = new PutWall(position, Wall.VERTICAL);
                         if(move.isLegal(board)) {
                             tempBoard.move(move);
-                            childern.add(new Pair<>(tempBoard, move));
+                            childern.add(new Node(tempBoard, move));
                         }
                     }
                 }
